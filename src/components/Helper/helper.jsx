30815439fad7d7e6ad869e3ed283b4e700da61b0
tejaswimvm <tejaswi.mvmbs@gmail.com>
2025-09-12 17:26:47 +0530

    Fgstmails complete

--- src/components/Helper/helper.jsx
+++ src/components/Helper/helper.jsx
@@ -1,3 +1,3 @@
 // export const base_url="http://localhost:4001"
- export const base_url="https://api.dailynewsmail.com"
- export const base_url1="https://user.fgstmails.com"
+export const base_url = "https://api.fgstmails.com";
+// export const base_url1 = "https://api.fgstmails.com/";
