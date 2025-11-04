23196f297b3a552c506d860a1775f2b9b6530236
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-11-04 16:30:31 +0530

    popup added

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { ArticleHome } from "./ArticleHome";
+import EVPopup from "../popup/page";
 const Article = ({ data }) => {
   return (
     <div>
+      <EVPopup data={data}/>
       <ArticleHome data={data} />
     </div>
   );
